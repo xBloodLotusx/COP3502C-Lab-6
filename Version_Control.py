39e34b49ea0ac52c551d1b01fd2fd0df48a3584f
Joshie Land <joshie@Joshies-MacBook-Pro.local>
2023-10-26 10:42:40 -0400

    First Commit

---
+++ Version_Control.py
@@ -0,0 +1,23 @@
+# Joshua Austin M Land
+alpha = True
+def menu():
+    print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+    # menu function
+
+
+
+while alpha:
+    menu()
+    omega = input('Please enter an option: ')
+    if omega == '1':
+        zeta = input('Please enter your password to encode: ')
+        theta = ''
+        for char in zeta:
+            if char.isdigit():
+                theta += str(int(char) + 3)
+            else:
+                theta += char
+        print('Your password has been encoded and stored!')
+
+    elif omega == '3':
+        break
