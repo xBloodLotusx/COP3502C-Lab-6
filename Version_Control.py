92275272d9c1d9ec46cf09aee6264fb14ebf5971
Manas Borkar <mborkar1225@gmail.com>
2023-10-31 14:11:43 -0400

    added decoder

--- Version_Control.py
+++ Version_Control.py
@@ -1,25 +1,47 @@
-# Joshua Austin M Land
 alpha = True
+
 def menu():
     print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+
     # menu function
-def encode():
-    if omega == '1':
-        zeta = input('Please enter your password to encode: ')
+
+    # Joshua Austin M Land
+def encode(zeta):
+
         theta = ''
         for char in zeta:
             if char.isdigit():
                 theta += str(int(char) + 3)
             else:
                 theta += char
+
         print('Your password has been encoded and stored!')
-    return theta
+        return theta
+
+# Manas Borkar
+def decode(theta):
+
+    delta = ''
+    for char in theta:
+        if char.isdigit():
+            delta += str(int(char) - 3)
+        else:
+            delta += char
+    print(f"The encoded password is {theta}, and the original is {delta}")
+    return delta
 
 
-while alpha:
-    menu()
-    omega = input('Please enter an option: ')
-    if omega == '1':
-        theta = encode()
-    elif omega == '3':
-        break
+def main():
+    while alpha:
+        menu()
+        omega = input("Please enter an option:")
+        if omega == '1':
+            zeta = input('Please enter your password to encode: ')
+            theta = encode(zeta)
+        if omega == '2':
+            decode(theta)
+        if omega == '3':
+            break
+
+if __name__ == '__main__':
+    main()
